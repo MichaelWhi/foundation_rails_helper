d3ba10ae01d40d3d00c6d428d097c32c165536c2
Michael Whittaker <post@michael-whittaker.de>
2012-08-31 23:54:19 +0200

    adds helper for badges and info labels

--- lib/foundation_rails_helper/foundation_helper.rb
+++ lib/foundation_rails_helper/foundation_helper.rb
@@ -8,5 +8,23 @@ module FoundationRailsHelper
 
       content_tag(:i, nil,  options) + " "
     end
+    
+    def badge_tag(text, options={})
+      klass = ["round label"]
+      klass << options.delete(:class)
+      klass = klass.join( ' ' )
+      options = options.merge({:class => klass})
+
+      content_tag(:span, text,  options) + " "
+    end
+    
+    def info_tag(text, options = {})
+      klass = ["radius label"]
+      klass << options.delete(:class)
+      klass = klass.join( ' ' )
+      options = options.merge({:class => klass})
+
+      content_tag(:span, text,  options) + " "
+    end
   end
 end
